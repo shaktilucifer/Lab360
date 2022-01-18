9801c1bd2ebd10557188b3078b79cc8d569552ca
Shakti <shakticse@gmail.com>
2022-01-17 23:37:27 -0500

    add some flat ui colors

--- freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
+++ freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Drumblock.css'
-export function DrumBlock({drumName, drumSound}: DrumBlockProps) {
+export function DrumBlock({drumName, drumSound, colorClass}: DrumBlockProps) {
     return <div className='block'>{drumName},{drumSound}</div>
 }
 
 interface DrumBlockProps {
     drumName: string;
     drumSound?: string;
+    colorClass?: string;
 }
