d28cff7e3ebd10e9b40d5c938315f10f9948e2a7
Shakti <shakticse@gmail.com>
2022-01-20 21:17:49 -0500

    try audio on click

--- freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
+++ freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import './Drumblock.css'
-export function DrumBlock({drumName, drumSound, colorClass}: DrumBlockProps) {
+import React from "react";
+import "./Drumblock.css";
+export function DrumBlock({ drumName, drumSound, colorClass }: DrumBlockProps) {
+  const playDrum = () => {
+    // play sound
+    console.log('plau sounds;')
+    var music = new Audio("/sounds/good-open-hat-sample-a-key-01-Kl9.mp3");
+    music.play();
+  };
 
-    const playDrum = () => {
-        // play sound
-    }
-
-    return (<div>
-        <button onClick={() => playDrum} className={`block ${colorClass}`}>{drumName},{drumSound}</button>
-    </div>);
+  return (
+    <div>
+      <button onClick={playDrum} className={`block ${colorClass}`}>
+        {drumName},{drumSound}
+      </button>
+    </div>
+  );
 }
 
 interface DrumBlockProps {
-    drumName: string;
-    drumSound?: string;
-    colorClass?: string;
+  drumName: string;
+  drumSound?: string;
+  colorClass?: string;
 }
