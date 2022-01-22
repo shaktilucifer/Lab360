f4d910513d1d623c69bc715b9f0e068e88a22de0
Shakti <shakticse@gmail.com>
2022-01-21 23:10:32 -0500

    add typescript resolver for mp3  files

--- freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
+++ freecodecamp/frontend/DrumMachine/drummachine/src/DrumBlock.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Drumblock.css";
+import audioMp3 from "./sounds/good-open-hat-sample-a-key-01-Kl9.mp3";
 export function DrumBlock({ drumName, drumSound, colorClass }: DrumBlockProps) {
   const playDrum = () => {
     // play sound
-    console.log('plau sounds;')
-    var music = new Audio("/sounds/good-open-hat-sample-a-key-01-Kl9.mp3");
+    console.log("plau sounds;");
+    var music = new Audio(audioMp3);
     music.play();
   };
 
