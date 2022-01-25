206bdbb07a11ac1ae29186af1a51a644ed1c3f96
Shakti <shakticse@gmail.com>
2022-01-24 23:14:24 -0500

    more flat ui colors

--- freecodecamp/frontend/DrumMachine/drummachine/src/constants/appConstant.ts
+++ freecodecamp/frontend/DrumMachine/drummachine/src/constants/appConstant.ts
@@ -8,15 +8,18 @@ export enum DRUM_COLORS {
     WISTERIA = 'wisteria', 
     MIDNIGHT = 'midnight-blue',
     BELIZE = 'belize',
+    EMERALD = 'emerald',
+    ALZARIN = 'alzarin',
+    POMEGRANATE = 'pomegranate',
 }
 
 export const DRUMS: Drums[] = [
     {name: 'drum1', color: DRUM_COLORS.BELIZE, audio: goodOpenHat},
     {name: 'drum2', color: DRUM_COLORS.MIDNIGHT, audio: sharpOpenHat},
     {name: 'drum3', color: DRUM_COLORS.WISTERIA, audio: subtleHiHat}, 
-    {name: 'drum4', color: DRUM_COLORS.MIDNIGHT, audio: sharpOpenHat},
-    {name: 'drum5', color: DRUM_COLORS.WISTERIA, audio: subtleHiHat},
-    {name: 'drum6', color: DRUM_COLORS.BELIZE, audio: goodOpenHat}
+    {name: 'drum4', color: DRUM_COLORS.EMERALD, audio: sharpOpenHat},
+    {name: 'drum5', color: DRUM_COLORS.ALZARIN, audio: subtleHiHat},
+    {name: 'drum6', color: DRUM_COLORS.POMEGRANATE, audio: goodOpenHat}
 ]
 
 export interface Drums {
