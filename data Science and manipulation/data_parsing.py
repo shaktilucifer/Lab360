151cc3a5fd0301feb023eb04764d6f7f9065f940
Shakti <shakticse@gmail.com>
2022-03-10 22:54:02 -0500

    add parser

---
+++ data Science and manipulation/data_parsing.py
@@ -0,0 +1,4 @@
+# // Do stuff with pokemon data 
+# // find pokemon with highest aggregate base stats
+# // Find pokemon in each type with highest aggregate base stats 
+# // Form pokemon tiers with max weaknesses to min weaknesses
