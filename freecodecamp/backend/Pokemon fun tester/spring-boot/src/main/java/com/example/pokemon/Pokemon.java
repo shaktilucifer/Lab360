b2cfeb32534b1585abff24ab407e00f1a03fddd5
Shakti <shakticse@gmail.com>
2022-03-28 23:37:46 -0400

    build

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/Pokemon.java
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/Pokemon.java
@@ -1,55 +1,57 @@
+package com.example.pokemon;
+
 class Pokemon {
-    class Ability {
-        private String name;
-        private Type type;
+//     class Ability {
+//         private String name;
+//         private Type type;
 
-        public Ability(String name, Type type) {
-            this.name = name;
-            this.type = type;
-        }
-    }
+//         public Ability(String name, Type type) {
+//             this.name = name;
+//             this.type = type;
+//         }
+//     }
 
-    class Move {
-        private String name;
-        private Type type;
+//     class Move {
+//         private String name;
+//         private Type type;
 
-        public Move(String name, Type type) {
-            this.name = name;
-            this.type = type;
-        }
-    }
+//         public Move(String name, Type type) {
+//             this.name = name;
+//             this.type = type;
+//         }
+//     }
 
-    private enum Type {
-        ELECTRIC,
-        WATER,
-        PSYCHIC,
-        GHOST,
-        DARK,
-        FIRE,
-        GRASS,
-        FLYING,
-        FAIRY,
-        BUG,
-        STEEL,
-        ICE,
-        FIGHTING,
-        POISON,
-        ROCK,
-        DRAGON,
-        GROUND,
-        NORMAL
-    }
+//     private enum Type {
+//         ELECTRIC,
+//         WATER,
+//         PSYCHIC,
+//         GHOST,
+//         DARK,
+//         FIRE,
+//         GRASS,
+//         FLYING,
+//         FAIRY,
+//         BUG,
+//         STEEL,
+//         ICE,
+//         FIGHTING,
+//         POISON,
+//         ROCK,
+//         DRAGON,
+//         GROUND,
+//         NORMAL
+//     }
 
-    List<Ability> abilities;
-    List<Move> moves;
+//     List<Ability> abilities;
+//     List<Move> moves;
 
-    public Pokemon() {
-        this.abilities = new ArrayList<>();
-        this.moves = new ArrayList<>();
-    }
+//     public Pokemon() {
+//         this.abilities = new ArrayList<>();
+//         this.moves = new ArrayList<>();
+//     }
 
-    public Pokemon(List<Ability> abilities, List<Move> moves) {
-        this.abilities = abilities;
-        this.moves = moves;
-    }
+//     public Pokemon(List<Ability> abilities, List<Move> moves) {
+//         this.abilities = abilities;
+//         this.moves = moves;
+//     }
 }
