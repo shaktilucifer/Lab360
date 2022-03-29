2c39b6fe2d90536c86971edfc735b1828e297e08
Shakti <shakticse@gmail.com>
2022-03-28 23:34:23 -0400

    scaffold spring intializer

---
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/CsvParser.java
@@ -0,0 +1,4 @@
+package com.example.pokemon;
+class CsvParser {
+    // do some parsing on Pokemon Data next
+}
---
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/Pokemon.java
@@ -0,0 +1,55 @@
+class Pokemon {
+    class Ability {
+        private String name;
+        private Type type;
+
+        public Ability(String name, Type type) {
+            this.name = name;
+            this.type = type;
+        }
+    }
+
+    class Move {
+        private String name;
+        private Type type;
+
+        public Move(String name, Type type) {
+            this.name = name;
+            this.type = type;
+        }
+    }
+
+    private enum Type {
+        ELECTRIC,
+        WATER,
+        PSYCHIC,
+        GHOST,
+        DARK,
+        FIRE,
+        GRASS,
+        FLYING,
+        FAIRY,
+        BUG,
+        STEEL,
+        ICE,
+        FIGHTING,
+        POISON,
+        ROCK,
+        DRAGON,
+        GROUND,
+        NORMAL
+    }
+
+    List<Ability> abilities;
+    List<Move> moves;
+
+    public Pokemon() {
+        this.abilities = new ArrayList<>();
+        this.moves = new ArrayList<>();
+    }
+
+    public Pokemon(List<Ability> abilities, List<Move> moves) {
+        this.abilities = abilities;
+        this.moves = moves;
+    }
+}
