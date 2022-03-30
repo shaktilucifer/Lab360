453c62a30bd01e85e91b2c663dc8987a3a5c4d62
Shakti <shakticse@gmail.com>
2022-03-29 22:40:19 -0400

    Rought tests

---
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.pokemon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PokemonControllerTest {
+
+    @Autowired
+	private MockMvc mvc;
+
+    @Test
+    public void helloWordTest() {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("Greetings from Spring Boot!")));    }
+    
+    
+}
