ced01a520b30e6571305f54abc17eb4b19b8252f
Shakti <shakticse@gmail.com>
2022-03-30 22:45:56 -0400

    read list and get ability

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonController.java
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonController.java
@@ -1,6 +1,11 @@
 package com.example.pokemon;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +14,17 @@ public class PokemonController {
     @RequestMapping("/")
     public String helloWorld(){
       return "Hello World from Spring Boot";
+    }
+
+    @GetMapping("/pokemonList") 
+    public List<Pokemon> getPokemonList(){
+      // do something to get list of pokemon maybe paged
+      return new LinkedList<>();
+    }
+
+    @GetMapping("/ability")
+    public List<Pokemon> getAbility(@RequestParam String id, @RequestParam(required = false) String pokemonName) {
+        return new LinkedList<>();
     }
     
 }
