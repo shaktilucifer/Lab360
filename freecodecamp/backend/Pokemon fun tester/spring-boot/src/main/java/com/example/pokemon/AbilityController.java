b72c0a9e96f21b680388eb6306c324e4f1b2cf00
Shakti <shakticse@gmail.com>
2022-04-16 23:23:24 -0400

    remove unused imports and cleanup

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/AbilityController.java
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/AbilityController.java
@@ -1,19 +1,10 @@
 package com.example.pokemon;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
-import com.opencsv.exceptions.CsvException;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonDataBuilder.java
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonDataBuilder.java
@@ -9,27 +9,28 @@ import javax.annotation.PostConstruct;
 
 public class PokemonDataBuilder {
 
-    public static final Integer POKEMON_NAME_COLUMN = 30;
-    public static final Integer POKEMON_HEADER_COLUMN = 1;
+  public static final Integer POKEMON_NAME_COLUMN = 30;
+  public static final Integer POKEMON_HEADER_COLUMN = 1;
 
-    static Map<String, String[]> pokemonDetailsMap = new HashMap<>();
-    static List<String[]> pokemonCSVList = new LinkedList<>();
-    static PokemonParserOpenCsv pokemonParserOpenCsv = PokemonParserOpenCsv.getInstance();
-    @PostConstruct
-    private void buildPokemonMap() throws Exception {
-      pokemonCSVList = pokemonParserOpenCsv.getRawPokemonData();
-      
-      for(String[] pokemon: pokemonCSVList) {
-        pokemonDetailsMap.put(pokemon[30].toLowerCase(), pokemon);
-      }
-    }
+  static Map<String, String[]> pokemonDetailsMap = new HashMap<>();
+  static List<String[]> pokemonCSVList = new LinkedList<>();
+  static PokemonParserOpenCsv pokemonParserOpenCsv = PokemonParserOpenCsv.getInstance();
 
-    public static Map<String, String[]> getPokemonMap() {
-        return pokemonDetailsMap;
+  @PostConstruct
+  private void buildPokemonMap() throws Exception {
+    pokemonCSVList = pokemonParserOpenCsv.getRawPokemonData();
+
+    for (String[] pokemon : pokemonCSVList) {
+      pokemonDetailsMap.put(pokemon[30].toLowerCase(), pokemon);
     }
-    
-    public static List<String[]> getPokemonList() {
-        return pokemonCSVList;
-    }
-    
+  }
+
+  public static Map<String, String[]> getPokemonMap() {
+    return pokemonDetailsMap;
+  }
+
+  public static List<String[]> getPokemonList() {
+    return pokemonCSVList;
+  }
+
 }
