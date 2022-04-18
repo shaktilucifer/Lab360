faef2428740fa30782f09592a687ac5eeb3cdbf4
Shakti <shakticse@gmail.com>
2022-04-18 00:02:24 -0400

    webclient json research

---
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonData.java
@@ -0,0 +1,8 @@
+package com.example.pokemon;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PokemonData {
+    
+}
--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonParserPokeApi.java
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/main/java/com/example/pokemon/PokemonParserPokeApi.java
@@ -10,31 +10,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-class PokemonParserPokeApi implements PokemonDataParser{
+class PokemonParserPokeApi implements PokemonDataParser {
 
-    private final static String POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=1126/";
+  private final static String POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=1126/";
 
-    @Override
-    public List<Pokemon> getPokemonList() {
-      WebClient webClient = WebClient.create(POKE_API);
-      webClient.get()
-      .uri("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
-      .retrieve();
-      
-        RestTemplate restTemplate = new RestTemplate();
-        String fooResourceUrl
-          = "https://pokeapi.co/api/v2/pokemon?limit=1126/";
-        ResponseEntity<String> response
-          = restTemplate.getForEntity(fooResourceUrl, String.class);
-        return null;
-    }
+  @Override
+  public List<Pokemon> getPokemonList() {
+    WebClient webClient = WebClient.create(POKE_API);
+    webClient.get()
+        .uri(POKE_API)
+        .retrieve();
 
-    @Override
-    public Map<String, String> getPokemonAbilities() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    RestTemplate restTemplate = new RestTemplate();
+    String fooResourceUrl = "https://pokeapi.co/api/v2/pokemon?limit=1126/";
+    ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+    return null;
+  }
 
-
+  @Override
+  public Map<String, String> getPokemonAbilities() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
