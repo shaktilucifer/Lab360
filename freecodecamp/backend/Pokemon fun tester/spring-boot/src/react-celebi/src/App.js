ae00d7e1a2a74e0ec82fdd04306e654d5b973660
Shakti <shakticse@gmail.com>
2022-04-26 20:17:17 -0400

    test api call

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/App.js
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/App.js
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+  useEffect(() => {
+    axios.get('localhost:8080/pokemons').then((response) => console.log(response)).catch((error) => console.log(error));9
+  }, []); 
+
   return (
     <div className="App">
       <header className="App-header">
