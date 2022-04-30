21fdcb717d87c930bb0d0c0d22db0f4a2b1d818a
Shakti <shakticse@gmail.com>
2022-04-29 23:39:04 -0400

    clear app.js

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/App.js
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/App.js
@@ -2,17 +2,14 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { PokemonDetail } from './PokemonDetail';
 
 
 function App() {
-
-  useEffect(() => {
-    axios.get('localhost:8080/pokemons').then((response) => console.log(response)).catch((error) => console.log(error));9
-  }, []); 
-
   return (
     <div className="App">
         <h2>App Test</h2>
+        <PokemonDetail />
     </div>
   );
 }
