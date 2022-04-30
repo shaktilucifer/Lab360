782ed8ccfe6bac50a149bfa3ec5134b67f8b2367
Shakti <shakticse@gmail.com>
2022-04-29 23:39:19 -0400

    audio file stuff

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
@@ -1,5 +1,55 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import ReactPlayer from 'react-player'
+import {Howl, Howler} from 'howler';
+import { Media, Player, controls, utils } from 'react-media-player'
+const {
+  PlayPause,
+  CurrentTime,
+  Progress,
+  SeekBar,
+  Duration,
+  MuteUnmute,
+  Volume,
+  Fullscreen,
+} = controls
+const { keyboardControls } = utils
 export function PokemonDetail() {
-    return <div></div>;
+    useEffect(() => {
+        console.log('useeffect');
+
+    }, [])
+    return (
+    <div>
+         <audio
+        controls
+        src="">
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio>
+    <ReactAudioPlayer
+  src="my_audio_file.ogg"
+  autoPlay
+  controls
+/>
+<Media>
+        {mediaProps => (
+          <div
+            className="media"
+            onKeyDown={keyboardControls.bind(null, mediaProps)}
+          >
+            <Player src="" className="media-player" />
+            <div className="media-controls">
+              <PlayPause />
+              <CurrentTime />
+              <SeekBar />
+              <Duration />
+              <MuteUnmute />
+              <Volume />
+              <Fullscreen />
+            </div>
+          </div>
+        )}
+      </Media>
+</div>);
 }
