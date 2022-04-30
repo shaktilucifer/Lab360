348c324bf387ed7ba05d427ea4f78d4396ff48bb
Shakti <shakticse@gmail.com>
2022-04-30 15:02:40 -0400

    remove audio player stuff

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
@@ -1,55 +1,11 @@
-import React, { useEffect } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
-import ReactPlayer from 'react-player'
-import {Howl, Howler} from 'howler';
-import { Media, Player, controls, utils } from 'react-media-player'
-const {
-  PlayPause,
-  CurrentTime,
-  Progress,
-  SeekBar,
-  Duration,
-  MuteUnmute,
-  Volume,
-  Fullscreen,
-} = controls
-const { keyboardControls } = utils
+import React, { useEffect, useState } from "react";
+import "./PokemonDetail.css";
 export function PokemonDetail() {
-    useEffect(() => {
-        console.log('useeffect');
+  useEffect(() => {
 
-    }, [])
-    return (
+  });
+  return (
     <div>
-         <audio
-        controls
-        src="">
-            Your browser does not support the
-            <code>audio</code> element.
-    </audio>
-    <ReactAudioPlayer
-  src="my_audio_file.ogg"
-  autoPlay
-  controls
-/>
-<Media>
-        {mediaProps => (
-          <div
-            className="media"
-            onKeyDown={keyboardControls.bind(null, mediaProps)}
-          >
-            <Player src="" className="media-player" />
-            <div className="media-controls">
-              <PlayPause />
-              <CurrentTime />
-              <SeekBar />
-              <Duration />
-              <MuteUnmute />
-              <Volume />
-              <Fullscreen />
-            </div>
-          </div>
-        )}
-      </Media>
-</div>);
+    </div>
+  );
 }
