c0bb60da77df18135b16f54a5a770f627481f36d
Shakti <shakticse@gmail.com>
2022-05-01 22:08:11 -0400

    Basic pokemon detail page

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
@@ -1,11 +1,21 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./PokemonDetail.css";
 export function PokemonDetail() {
+  const pokemon = {};
   useEffect(() => {
-
+    //   axios.get('')
+    pokemon = {
+      pokemonName: "pikachu",
+      pokemonType: ["electric", "fairy"],
+      hitpoints: 100,
+    };
   });
   return (
     <div>
+      <div>{pokemon.pokemonName}</div>
+      <div>{pokemon.pokemonType}</div>
+      <div>{pokemon.hitpoints}</div>
     </div>
   );
 }
