59458d3c734af3c0b56e8e58476ab4233d90c8d6
Shakti <shakticse@gmail.com>
2022-05-01 22:40:44 -0400

    list pokemon names on UI

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/PokemonDetail.jsx
@@ -2,20 +2,17 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./PokemonDetail.css";
 export function PokemonDetail() {
-  let pokemon = {};
-  useEffect(() => {
-      axios.get('http://localhost:8080/pokemons').then((response) => console.log(response));
-    pokemon = {
-      pokemonName: "pikachu",
-      pokemonType: ["electric", "fairy"],
-      hitpoints: 100,
-    };
-  });
+    const [pokemons, setPokemons] = useState([]);
+    useEffect(() => {
+      axios.get('http://localhost:8080/pokemons').then((response) => {
+          setPokemons(response.data);
+      });
+  }, []);
   return (
     <div>
-      <div>{pokemon.pokemonName}</div>
-      <div>{pokemon.pokemonType}</div>
-      <div>{pokemon.hitpoints}</div>
+        {pokemons.map((pokemon)=> {
+            return <div>{pokemon.name}</div>
+        })}
     </div>
   );
 }
