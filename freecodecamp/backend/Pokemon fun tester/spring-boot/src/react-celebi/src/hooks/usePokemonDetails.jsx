e9adf7e67d32eb9c966f13c3ebf462b8929b4a29
Shakti <shakticse@gmail.com>
2022-05-07 23:17:47 -0400

    use pokemon details

--- freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/hooks/usePokemonDetails.jsx
+++ freecodecamp/backend/Pokemon fun tester/spring-boot/src/react-celebi/src/hooks/usePokemonDetails.jsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import axios from "axios";
 import BASE_URL from "../constants/app.conf";
-export function usePokemonList() {
+export function usePokemonDetails() {
   return useCallback((pokemonName) =>{
     return axios.get(`${BASE_URL}/pokemon?name=${pokemonName}`);
   })
